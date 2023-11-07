5594728454c66bdc82f56e81152f2380e64762e5
mdalmamunit427 <mdalmamunit427@gmail.com>
2023-11-07 10:39:11 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <h1 className='text-4xl font-bold'>MERN Stack <span className='text-indigo-700'>Job Portal</span> Starter Files</h1>
+    </div>
+  )
+}
+
+export default App
